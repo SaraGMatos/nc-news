2a5b4efae468f86f00707953dadc68c54540f09d
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-05 14:43:43 +0100

    Updates fetchArticles to include pagination

---
+++ api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const ncNewsApi = axios.create({
+  baseURL: "https://nc-news-app-jnwm.onrender.com/api",
+});
+
+export function fetchArticles(currentPage) {
+  return ncNewsApi.get("/articles", {
+    params: {
+      p: `${currentPage}`,
+    },
+  });
+}
