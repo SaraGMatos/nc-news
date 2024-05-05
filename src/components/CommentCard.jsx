b0db785022a63a473e3df109a1b13914a78cd78f
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-05 20:51:52 +0100

    Adds basic functionality to voting buttons

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 function CommentCard({ comment }) {
+  const [currentVotes, setCurrentVotes] = useState(comment.votes);
+  console.log(currentVotes);
+  function handleUpButton() {
+    return setCurrentVotes((currVotes) => {
+      console.log("hi up");
+      return currVotes + 1;
+    });
+  }
+
+  function handleDownButton() {
+    return setCurrentVotes((currVotes) => {
+      console.log("hi down");
+      return currVotes - 1;
+    });
+  }
+
   return (
     <section className="comment-card-container">
       <div className="comment-card-header">
@@ -7,9 +25,13 @@ function CommentCard({ comment }) {
       </div>
       <p className="comment-card-body">{comment.body}</p>
       <div className="comment-votes">
-        <button className="comment-vote-button">+</button>
-        <p>{comment.votes}</p>
-        <button className="comment-vote-button">-</button>
+        <button className="comment-vote-button" onClick={handleUpButton}>
+          +
+        </button>
+        <p>{currentVotes}</p>
+        <button className="comment-vote-button" onClick={handleDownButton}>
+          -
+        </button>
       </div>
     </section>
   );
