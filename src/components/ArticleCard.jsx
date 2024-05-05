84f46e691d42d876f798d4171e16e89075b9718a
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-05 17:35:34 +0100

    Adds Link component to ArticleCard component to allow SingleArticle view

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,22 +1,26 @@
+import { Link } from "react-router-dom";
+
 function ArticleCard({ article }) {
   return (
-    <div className="article-card">
-      <div className="article-card-img">
-        <img src={article.article_img_url} />
-      </div>
-
-      <div className="article-card-content">
-        <div className="article-card-topic-author">
-          <p>{article.topic}</p>
-          <p>{article.author}</p>
+    <Link to={`/articles/${article.article_id}`}>
+      <div className="article-card">
+        <div className="article-card-img">
+          <img src={article.article_img_url} />
         </div>
-        <h3>
-          {article.title.length < 56
-            ? article.title
-            : article.title.slice(0, 56) + "..."}
-        </h3>
+
+        <div className="article-card-content">
+          <div className="article-card-topic-author">
+            <p>{article.topic}</p>
+            <p>{article.author}</p>
+          </div>
+          <h3>
+            {article.title.length < 56
+              ? article.title
+              : article.title.slice(0, 56) + "..."}
+          </h3>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
