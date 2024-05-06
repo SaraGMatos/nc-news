4234d8d752784133e06e70d76ad9f2eca2801759
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-06 13:12:59 +0100

    Adds voting functionality to SingleArticle component

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -7,11 +7,13 @@ import { useContext } from "react";
 import { UserContext } from "../contexts/User";
 import Expandable from "./Expandable";
 import ArticleComments from "./ArticleComments";
+import { updateArticleByArticleId } from "../../api";
 
 function SingleArticle() {
   const [article, setArticle] = useState({});
   const { user } = useContext(UserContext);
   const { id } = useParams();
+  const [voteUpdate, setVoteUpdate] = useState(0);
 
   function getArticleById() {
     fetchArticleById(id).then((articleData) => {
@@ -21,12 +23,37 @@ function SingleArticle() {
   }
   useEffect(getArticleById, [id]);
 
+  function handleVote(vote) {
+    updateArticleByArticleId(vote, id).then(() => {
+      setVoteUpdate((currVoteUpdate) => {
+        return vote + currVoteUpdate;
+      });
+    });
+  }
+
   return (
     <>
       <Header user={user} />
       <SingleArticleHeader article={article} />
       <img className="article-image" src={article.article_img_url} />
       <section className="article-body">{article.body}</section>
+      <div className="article-votes">
+        <button
+          className="article-vote-button"
+          disabled={voteUpdate === 1}
+          onClick={() => handleVote(1)}
+        >
+          +
+        </button>
+        <p>{String(article.votes + voteUpdate)}</p>
+        <button
+          className="article-vote-button"
+          disabled={voteUpdate === -1}
+          onClick={() => handleVote(-1)}
+        >
+          -
+        </button>
+      </div>
       <Expandable id={id}>
         <ArticleComments id={id} />
       </Expandable>
